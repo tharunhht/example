06c0110b9b9b13faf6e224591c785cb3bf0d5129
tharun.rt <tharun.rt@CLAP3289>
2022-11-02 14:29:50 +0530

    Add project files.

---
+++ search/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="search.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Search" OnClick="Button1_Click" />
+        <asp:Label ID="msg" runat="server" Text=""></asp:Label>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="Name" HeaderText="Name" />
+                <asp:BoundField DataField="City" HeaderText="City" />
+                <asp:BoundField DataField="Gender" HeaderText="Gender" />
+            </Columns>
+        </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ search/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace search
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+
+
+
+            SqlCommand cmd = new SqlCommand("select * from search1 where name ='" + TextBox1.Text + "'", con);
+
+
+            SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+
+
+            DataTable dt = new DataTable();
+
+
+            con.Open(); adpt.Fill(dt);
+
+
+            if (dt.Rows.Count > 0)
+            {
+                GridView1.DataSource = dt; GridView1.DataBind();
+
+
+                msg.Text = "Record Founded!";
+
+
+            }
+            else { msg.Text = "Record Not Founded!"; }
+
+
+        }
+    }
+    }
